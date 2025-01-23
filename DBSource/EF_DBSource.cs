8d5edb178eb9025eed2ddaeb9cf1c7671c1a71ec
Sandro Vecchiarelli <sandro.vecchiarelli@pomiager.com>
2025-01-23 17:24:29 +0100

    db source

---
+++ DBSource/EF_DBSource.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSource
+{
+    public class EF_DBSource
+    {
+        public List<Asset_DTO> GetAllAssets()
+        {
+
+        }
+
+        public Asset_DTO GetAssetByID(int assetID)
+        {
+
+        }
+
+        public List<Asset_DTO> GetPagedAssets(int pagenumber, int pagesize)
+        {
+
+        }
+        public List<AssetDocument_DTO> GetAssetDocumentsByAssetID(int assetID)
+        {
+
+        }
+
+
+    }
+
+    public class Asset_DTO
+    {
+        public Asset_DTO()
+        {
+            Documents = new List<AssetDocument_DTO>();
+        }
+        public int IdAsset { get; set; }
+
+        public string AssetName { get; set; }
+
+        public string AssetType { get; set; }
+
+        public List<AssetDocument_DTO> Documents { get; set; }
+    }
+    public class AssetDocument_DTO
+    {
+        public int IdAssetDocument { get; set; }
+
+        public string AssetDocumentName { get; set; }
+
+        public string AssetDocumentType { get; set; }
+    }
+}
--- EntityFrameworkTest/Program.cs
+++ EntityFrameworkTest/Program.cs
@@ -8,17 +8,17 @@ namespace EntityFrameworkTest
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
 
             var employee = new { FirstName = "Mario", BirthDate = DateTime.Now.AddYears(-20) };
             //TestSelect();
             //TestInsert();
             //OldTest();
-            LinqSamples();
+            await LinqSamples();
         }
 
-        private static async void LinqSamples()
+        private static async Task LinqSamples()
         {
             AvioAssetChainDBContext _context = new AvioAssetChainDBContext();
             #region example 1
@@ -26,10 +26,10 @@ namespace EntityFrameworkTest
                                             .Include(a => a.AssetDocuments)
                                             .ToListAsync();
 
-            foreach (var asset in assetsWithDocuments)
+            foreach (var asset_ef in assetsWithDocuments)
             {
-                Console.WriteLine($"Asset: {asset.AssetName}");
-                foreach (var document in asset.AssetDocuments)
+                Console.WriteLine($"Asset: {asset_ef.AssetName}");
+                foreach (var document in asset_ef.AssetDocuments)
                 {
                     Console.WriteLine($" - Documento: {document.AssetDocumentName}");
                 }
@@ -42,11 +42,100 @@ namespace EntityFrameworkTest
                     .Where(a => a.AssetDocuments.Any(d => d.AssetDocumentType.ToUpper() == "PDF"))
                     .ToListAsync();
 
-            foreach (var asset in filteredAssets)
+            foreach (var asset_ef in filteredAssets)
             {
-                Console.WriteLine($"Asset con documenti PDF: {asset.AssetName}");
+                Console.WriteLine($"Asset con documenti PDF: {asset_ef.AssetName}");
             }
             #endregion
+            #region example 3
+            //3. Selezione con proiezione su DTO (Data Transfer Object)
+
+            var assetDtos = await _context.Assets
+                            //.Include(a => a.AssetDocuments)
+                            .Select(a => new
+                            {
+                                a.AssetName,
+                                a.AssetType,
+                                Documents = a.AssetDocuments.Select(d => new
+                                {
+                                    d.AssetDocumentName,
+                                    d.AssetDocumentType
+                                }).ToList()
+                            })
+                            .ToListAsync();
+
+            foreach (var asset_dto in assetDtos)
+            {
+                Console.WriteLine($"Asset: {asset_dto.AssetName}, Tipo: {asset_dto.AssetType}");
+                foreach (var doc_dto in asset_dto.Documents)
+                {
+                    Console.WriteLine($" - {doc_dto.AssetDocumentName} ({doc_dto.AssetDocumentType})");
+                }
+            }
+
+            #endregion
+
+
+            #region example 4
+            //4. Ricerca di un Asset specifico con i documenti collegati (Lazy Loading)
+
+            var asset = await _context.Assets.FindAsync(55);
+            _context.Entry(asset).Collection(a => a.AssetDocuments).Load();
+
+            Console.WriteLine($"Asset: {asset.AssetName}");
+            foreach (var doc in asset.AssetDocuments)
+            {
+                Console.WriteLine($" - {doc.AssetDocumentName}");
+            }
+
+            #endregion
+
+            #region example 5
+            var groupedData = await _context.Assets
+                .GroupBy(a => a.AssetType)
+                .Select(g => new
+                {
+                    AssetType = g.Key,
+                    DocumentCount = g.Sum(a => a.AssetDocuments.Count)
+                })
+                .ToListAsync();
+
+            foreach (var group in groupedData)
+            {
+                Console.WriteLine($"Tipo di Asset: {group.AssetType}, Numero di Documenti: {group.DocumentCount}");
+            }
+
+            #endregion
+
+            #region delete sample
+            var assetToDelete = await _context.Assets
+                .Include(a => a.AssetDocuments)
+                .FirstOrDefaultAsync(a => a.IdAsset == 54);
+
+            if (assetToDelete != null)
+            {
+                _context.Assets.Remove(assetToDelete);
+                await _context.SaveChangesAsync();
+                Console.WriteLine("Asset e documenti associati eliminati.");
+            }
+            #endregion
+
+            #region paging result
+            int pageSize = 5;
+            int pageNumber = 1;
+
+            var paginatedAssets = await _context.Assets
+                .OrderBy(a => a.AssetName)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            foreach (var asset_ef in paginatedAssets)
+            {
+                Console.WriteLine($"Asset: {asset.AssetName}");
+            }
+            #endregion
+
 
         }
 
