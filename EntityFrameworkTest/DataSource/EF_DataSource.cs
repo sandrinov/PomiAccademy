28991f3ee28462209b631457372fe008cb0c771e
Sandro Vecchiarelli <sandro.vecchiarelli@pomiager.com>
2025-01-24 13:04:59 +0100

    almost done ef_datasource

--- EntityFrameworkTest/DataSource/EF_DataSource.cs
+++ EntityFrameworkTest/DataSource/EF_DataSource.cs
@@ -1,6 +1,7 @@
 ﻿using EntityFrameworkTest.EF;
 using EntityFrameworkTest.Mappers;
 using EntityFrameworkTest.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,19 +26,34 @@ namespace EntityFrameworkTest.DataSource
 
             foreach (Asset asset in assets)
             {
-                result_list.Add(Mapper.From_EF_to_DTO(asset));
+                result_list.Add(Mapper.From_EFAsset_to_DTOAsset(asset));
             }
             return result_list;
         }
 
         public Asset_DTO GetAssetByID(int assetID)
         {
-            throw new NotImplementedException();
+            Asset_DTO result_dto = null;
+            Asset asset = _context.Assets.Where(a=>a.IdAsset == assetID).FirstOrDefault();
+            if (asset != null)
+                result_dto = Mapper.From_EFAsset_to_DTOAsset(asset);
+
+            return result_dto;
         }
 
         public List<AssetDocument_DTO> GetAssetDocumentsByAssetID(int assetID)
         {
-            throw new NotImplementedException();
+            List<AssetDocument_DTO> resultList = new List<AssetDocument_DTO>();
+            Asset asset = _context.Assets.Include(a=> a.AssetDocuments).Where(a => a.IdAsset == assetID).FirstOrDefault();
+            if (asset != null)
+            {
+                List<AssetDocument> ef_assetdocuments = asset.AssetDocuments.ToList();
+                foreach(AssetDocument ef_document in ef_assetdocuments)
+                {
+                    resultList.Add(Mapper.From_EFAssetDocument_to_DTOAssetDocument(ef_document));
+                }
+            }               
+            return resultList;
         }
 
         public List<Asset_DTO> GetPagedAssets(int pagenumber, int pagesize)
--- EntityFrameworkTest/Mappers/Mapper.cs
+++ EntityFrameworkTest/Mappers/Mapper.cs
@@ -9,13 +9,22 @@ namespace EntityFrameworkTest.Mappers
 {
     public class Mapper
     {
-        public static Asset_DTO From_EF_to_DTO(EntityFrameworkTest.EF.Asset ef_asset)
+        public static Asset_DTO From_EFAsset_to_DTOAsset(EntityFrameworkTest.EF.Asset ef_asset)
         {
             Asset_DTO result_dto = new Asset_DTO();
 
             result_dto.IdAsset = ef_asset.IdAsset;
             result_dto.AssetName = ef_asset.AssetName;
             result_dto.AssetType = ef_asset.AssetType;
+
+            return result_dto;
+        }
+        public static AssetDocument_DTO From_EFAssetDocument_to_DTOAssetDocument(EntityFrameworkTest.EF.AssetDocument ef_asset)
+        {
+            AssetDocument_DTO result_dto = new AssetDocument_DTO();
+            result_dto.IdAssetDocument = ef_asset.IdAssetDocument;
+            result_dto.AssetDocumentName = ef_asset.AssetDocumentName;
+            result_dto.AssetDocumentType = ef_asset.AssetDocumentType;
 
             return result_dto;
         }
